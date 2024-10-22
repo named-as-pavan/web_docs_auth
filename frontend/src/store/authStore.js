82267d43472e7937bcb97a5538d589803c550b6e
Pavan <pavan111222111@gmail.com>
2024-10-22 23:34:27 +0530

    implement backend url

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,9 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+// Define API_URL based on environment variables
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/auth";
+
 
 axios.defaults.withCredentials = true;
 
